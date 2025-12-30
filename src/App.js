77d85d7c910c07d544b593547eba6d7039bf7190
Pawan Nagar <pn583009@gmail.com>
2025-12-30 22:47:17 +0530

    Remove testing React

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import Home from './pages/Home';
 import ProjectDetails from './pages/ProjectDetails';
-import Debug from './Debug';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <Layout>
-        <Debug/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/project/:id" element={<ProjectDetails />} />
